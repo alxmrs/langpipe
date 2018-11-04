e1490632504168b26c85d1984081d1a6d910363c
Alex Rosengarten <alexrosengarten@gmail.com>
2018-11-04 23:13:59 +0100

    (made example script easier to read)

--- scripts/get_topics.sh
+++ scripts/get_topics.sh
@@ -1,7 +1,10 @@
 
-langpipe readlines -f ../raw_transcriptions_8000.txt \
-	sentences replace -f replacements.csv \
-	words filter-length-gte -n 3 \
-	remove-stopwords -f stop_words.txt \
-	filter-pos -p 'NN' -p 'NNP' -p 'NNS' -p 'NNPS' \
+langpipe \
+	readlines -f ../raw_transcriptions_8000.txt \
+	sentences \
+		replace -f replacements.csv \
+	words \
+		filter-length-gte -n 3 \
+		remove-stopwords -f stop_words.txt \
+		filter-pos -p 'NN' -p 'NNP' -p 'NNS' -p 'NNPS' \
 	trace
