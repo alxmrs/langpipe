0a3089612a533cc9da13267f517efd9868a2b0bf
Alex Rosengarten <alexrosengarten@gmail.com>
2018-11-04 22:49:53 +0100

    (updated scripts to test new features)

--- scripts/get_topics.sh
+++ scripts/get_topics.sh
@@ -1,2 +1,7 @@
 
-langpipe readlines -f ../raw_transcriptions_8000.txt sentences replace -f replacements.csv trace
+langpipe readlines -f ../raw_transcriptions_8000.txt \
+	sentences replace -f replacements.csv \
+	words filter-length-gte -m 3 \
+	remove-stopwords -f stop_words.txt trace \
+	filter-pos -p 'NN' -p 'NNP' -p 'NNS' -p 'NNPS' \
+	trace
